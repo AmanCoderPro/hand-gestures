f45726bc5a61fbee6488adf9ea3de7e671d0205b
AmanCoderPro <87359434+AmanCoderPro@users.noreply.github.com>
2022-09-02 17:41:12 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ Webcam.set({
     png_quality: 90
 });
 
+prediction = "";
+
 camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
@@ -17,5 +19,38 @@ function take_snapshot() {
 
 console.log('ml5 version: ', ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/LR-tH_Hdk/model.json");
+function modelLoaded() {
+    console.log('model Loaded');
+}
 
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/LR-tH_Hdk/model.json", modelLoaded);
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction = results[0].label;
+
+        if(results[0].label == "amzing") {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label == "best") {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "victory") {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+    }
+}
